89d3bb22ad981417942f94ed0d5e11f0e099604f
Shikut Ahammed <52927710+shykotahmed@users.noreply.github.com>
2019-09-02 00:05:35 +0600

    if,else condition

--- index.php
+++ index.php
@@ -1,36 +1,13 @@
 <?php
 
-$name="Shikut";
-$from="Prottoy";
 
-$balance=40.00;
+$balance=80.00;
 
-echo($from." has ".$balance." BDT <br/>" );
-
-// Prottoy has 40.00 BDT
-
-function addBalance($addBal){
-    $GLOBALS['balance']=($GLOBALS['balance']+$addBal);
-    echo($addBal." added <br/>");
-
+if($balance>=100){
+    echo("You have enough balance!");
+} else{
+    echo("Insufficient balance!!!");
 }
-
-function widraw($amount){
-    $GLOBALS['balance']=($GLOBALS['balance']-$amount);
-    echo($amount." widraw <br/>");
-}
-
-addBalance(100);
-echo($from." has ".$balance." BDT <br/>" );
-
-addBalance(500);
-echo($from." has ".$balance." BDT <br/>" );
-
-widraw(10);
-echo($from." has ".$balance." BDT <br/>" );
-
-widraw(30);
-echo($from." has ".$balance." BDT <br/>" );
 
 
 ?>

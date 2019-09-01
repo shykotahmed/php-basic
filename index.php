5bc7badb5681828f0433af007d3c07d3998ebd62
Shikut Ahammed <52927710+shykotahmed@users.noreply.github.com>
2019-09-02 00:00:19 +0600

    php function

--- index.php
+++ index.php
@@ -3,10 +3,34 @@
 $name="Shikut";
 $from="Prottoy";
 
-echo("Hello ".$name." from ".$from."<br/>");
-echo("Hello ".$name." from ".$from."<br/>");
-echo("Hello ".$name." from ".$from."<br/>");
+$balance=40.00;
 
+echo($from." has ".$balance." BDT <br/>" );
+
+// Prottoy has 40.00 BDT
+
+function addBalance($addBal){
+    $GLOBALS['balance']=($GLOBALS['balance']+$addBal);
+    echo($addBal." added <br/>");
+
+}
+
+function widraw($amount){
+    $GLOBALS['balance']=($GLOBALS['balance']-$amount);
+    echo($amount." widraw <br/>");
+}
+
+addBalance(100);
+echo($from." has ".$balance." BDT <br/>" );
+
+addBalance(500);
+echo($from." has ".$balance." BDT <br/>" );
+
+widraw(10);
+echo($from." has ".$balance." BDT <br/>" );
+
+widraw(30);
+echo($from." has ".$balance." BDT <br/>" );
 
 
 ?>
